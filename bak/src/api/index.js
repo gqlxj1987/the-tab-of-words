a5c9da97a6438fea3cb8d19f5fd0e9769e1254a7
gqlxj1987 <gqlxj1987@gmail.com>
2025-07-13 11:19:11 +0800

    feat: first commit

--- bak/src/api/index.js
+++
@@ -1,3 +0,0 @@
-const WORDS_JSON = './static/data/words.json'
-
-export const getWords = () => fetch(WORDS_JSON).then(data => data.json())
--- bak/src/utils/index.js
+++
@@ -1,7 +0,0 @@
-export const randomArrEl = (arr) => arr[Math.floor(Math.random() * arr.length)]
-
-export const getNextArrEl = (currentValue, arr) => {
-  const currentIdx = arr.indexOf(currentValue)
-  const nextIdx = currentIdx + 1 >= arr.length ? 0 : currentIdx + 1
-  return arr[nextIdx]
-}
